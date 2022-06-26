e483d6d0ebdb8f3790669633245b0514219c9605
temuj1n <68603105+temuj1n@users.noreply.github.com>
2022-06-26 17:37:45 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+$mot_de_passe = '****';
+if (isset($_POST['variable_du_formulaire']) AND $_POST['variable_du_formulaire'] == $mot_de_passe)
+{
+    header( 'Location: ./_mnemosyne/1-html/index.html' ); // Redirige vers l'accueil du site
+    exit();
+}
+?>
+<?php
+    // Vérification des champs vides ou incorrects et affichage du formulaire
+    $pass ='password';
+    if (!isset($_POST['pass']) OR $_POST['pass'] != $pass)
+    {
+?>
+<div>
+    <title>Archives photographiques</title>
+        <style>
+            div {
+                text-align: center;
+                 top: 50%;
+                 position: absolute;
+                 transform: translate(-50%,-50%);
+                left: 50%;
+                }
+        </style>
+    <p>Mot de passe nécessaire pour parcourir la base de données :
+    </p>
+    <form method="post" action="index.php">
+    <input type="password" name="pass" />
+    <input type="submit" value="Valider" />
+    </form>
+</div>
+<?php
+    }
+    else
+    {
+        header( 'Location: ./_mnemosyne/index.html' ); // Redirige vers l'accueil du site
+        exit();
+    }
+?>
---
+++ parse.php
@@ -0,0 +1,25 @@
+<?php
+
+	//get current directory
+	$working_dir = getcwd();
+
+    $images_dossier = $_GET['images_dossier'];
+	
+	//get image directory
+	$img_dir = $working_dir . "/" . $images_dossier . "/";
+	
+	//change current directory to image directory
+	chdir($img_dir);
+	
+	//using glob() function get images 
+	$files = glob("*.{jpg,jpeg,png,gif,JPG,JPEG,PNG,GIF}", GLOB_BRACE );
+	
+	//again change the directory to working directory
+	chdir($working_dir);
+
+	//iterate over image files
+	foreach ($files as $file) {
+	?>
+		<center><p><img src="<?php echo $images_dossier . "/" . $file ?>" style="height: auto; max-width: 50vw;"/></p></center>
+	<?php }
+?>
