2e36230b2b85f6a09c74e21178f619230fde5ce1
Nicolas Bellet <68603105+temuj1n@users.noreply.github.com>
2023-08-01 10:16:34 +0200

    Update isotope-personnes.js

--- _mnemosyne/js/isotope-personnes.js
+++ _mnemosyne/js/isotope-personnes.js
@@ -1,7 +1,10 @@
-// bellet-graber script 1.0
+// bellet-graber script 2.0
 // tri des albums par familles
 
-var $grid = $('.grid').isotope({
+// store filter per group
+var filters = {};
+
+var $container = $('.grid').isotope({
   itemSelector: '.element-item',
   layoutMode: 'masonry',
   masonry: {
@@ -9,18 +12,57 @@ var $grid = $('.grid').isotope({
   }
 });
 
-$('.filters-button-group').on( 'click', 'button', function() {
-  var filterValue = $(this).attr('data-filter');
-  $grid.isotope({ filter: filterValue });
-});
-
-$('.button-group').each( function( i, buttonGroup ) {
-  var $buttonGroup = $( buttonGroup );
-  $buttonGroup.on( 'click', 'button', function() {
-    $buttonGroup.find('.is-checked').removeClass('is-checked');
-    $( this ).addClass('is-checked');
-  });
-});
-
-
+$container.isotope();
+// do stuff when checkbox change
+$('.button-group').on( 'change', function( event ) {
+  var checkbox = event.target;
+  var $checkbox = $( checkbox );
+  var group = $checkbox.parents('.row').attr('data-group');
+  // create array for filter group, if not there yet
+  var filterGroup = filters[ group ];
+  if ( !filterGroup ) {
+    filterGroup = filters[ group ] = [];
+  }
+  // add/remove filter
+  if ( checkbox.checked ) {
+    // add filter
+    filterGroup.push( checkbox.value );
+  } else {
+    // remove filter
+    var index = filterGroup.indexOf( checkbox.value );
+    filterGroup.splice( index, 1 );
+  }
   
+  var comboFilter = getComboFilter();
+  $container.isotope({ filter: comboFilter });
+});
+
+
+
+function getComboFilter() {
+  var combo = [];
+  for ( var prop in filters ) {
+    var group = filters[ prop ];
+    if ( !group.length ) {
+      // no filters in group, carry on
+      continue;
+    }
+    // add first group
+    if ( !combo.length ) {
+      combo = group.slice(0);
+      continue;
+    }
+    // add additional groups
+    var nextCombo = [];
+    // split group into combo: [ A, B ] & [ 1, 2 ] => [ A1, A2, B1, B2 ]
+    for ( var i=0; i < combo.length; i++ ) {
+      for ( var j=0; j < group.length; j++ ) {
+        var item = combo[i] + group[j];
+        nextCombo.push( item );
+      }
+    }
+    combo = nextCombo;
+  }
+  var comboFilter = combo.join('');
+  return comboFilter;
+}
